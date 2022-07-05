dffef970636b7f74051920672d03bbf00bc9ae37
alvaro <alvarocondeorlando@gmail.com>
2022-07-04 23:48:04 -0300

    feat(migration): Implementada migration da entidade categorias

---
+++ src/database/migrations/20220705023711-create-categories.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+   up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable('categorias', {
+        id:{
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull: false
+        },
+        codigo:{
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        titulo:{
+          type: Sequelize.STRING,
+          allowNull: false
+        },
+        status: {
+          type: Sequelize.INTEGER,
+          allowNull: false
+        },
+        created_at: {
+          type: Sequelize.DATE,
+          allowNull: false
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false
+        }
+      })
+  },
+
+   down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable('categorias');
+  }
+};
