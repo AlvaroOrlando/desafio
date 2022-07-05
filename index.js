49240b69b219646b3df04625b3b30cb332a5b7f5
alvaro <alvarocondeorlando@gmail.com>
2022-07-04 22:06:15 -0300

    build: Criada estrutura geral do projeto

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+//enviroment variables
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'localhost';
+
+
+
+//server
+app.listen(PORT,HOST, _ =>{
+    console.log(`Server is listening ${HOST}:${PORT}`)
+})
+
