cf806ec18d7de5f14fc05fedb88ddcbb57781f75
alvaro <alvarocondeorlando@gmail.com>
2022-07-06 14:19:58 -0300

    feat(categorias): Criado endpoint DELETE/:id categorias

--- src/controllers/CategoriasController.js
+++ src/controllers/CategoriasController.js
@@ -3,24 +3,38 @@ const Categorias = require('../models/Categorias');
 module.exports = {
     async store(req, res) {
         const { codigo, titulo, status } = req.body;
-        const categorias = await Categorias.create({ codigo, titulo, status })
-
+        const categorias = await Categorias.create({ 
+            codigo, 
+            titulo, 
+            status })
         return res.json(categorias);
     },
+
     async getById(req,res) {
         const { id } = req.params;
         const categorias = await Categorias.findByPk(id);
-
         return res.json(categorias);
     },
+
     async getAll(req,res) {
         const categorias = await Categorias.findAll();
         return res.json(categorias);
     },
+
     async update(req,res) {
         const { id } = req.params;
         const { codigo, titulo, status } = req.body;
-        const categorias = await Categorias.update({ codigo, titulo, status }, { where: { id } });
+        const categorias = await Categorias.update({
+            codigo, 
+            titulo, 
+            status 
+        },{ where: { id } });
+        return res.json(categorias);
+    },
+
+    async delete(req,res) {
+        const { id } = req.params;
+        const categorias = await Categorias.destroy({ where: { id } });
         return res.json(categorias);
     }
 };
