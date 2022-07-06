7a2de5373ca810bb417185fd234ebaad6f647d91
alvaro <alvarocondeorlando@gmail.com>
2022-07-06 15:23:53 -0300

    refactor(database/sequelize) Refatoração database Sequelize

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,15 @@ const dbConfig = require('../config/database');
 const connection = new Sequelize(dbConfig);
 
 const Categorias = require('../models/Categorias');
+const Produtos = require('../models/Produtos');
+const Estoques = require('../models/Estoques');
+
 Categorias.init(connection);
+Produtos.init(connection);
+Estoques.init(connection);
+
+Produtos.associate(connection.models);
+Estoques.associate(connection.models);
 
 
 module.exports = connection;
